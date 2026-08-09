a9adfcb7143d72cfc4558f9d69039a9d47ba5ee2
AkshanshK04 <akshanshk0406@gmail.com>
2026-08-09 22:20:05 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.37%) - LeetHub

---
+++ 0046-permutations/0046-permutations.c
@@ -0,0 +1,48 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+void backtrack (int* nums,int numsSize, int* current, int currentSize, int* used, int** result, int* returnSize, int** returnColumnSizes){
+
+    if ( currentSize == numsSize)
+    {
+        result[*returnSize] = malloc(numsSize*sizeof(int));
+        for ( int i =0; i<numsSize; i++)
+            result[*returnSize][i] = current[i];
+        
+        (*returnColumnSizes)[*returnSize] =  numsSize;
+        (*returnSize)++;
+        return;
+    }
+
+    for ( int i=0; i<numsSize; i++)
+    {
+        if (used[i])
+            continue;
+        
+        used[i]= 1;
+        current[currentSize] = nums[i];
+        backtrack(nums, numsSize, current, currentSize +1, used, result, returnSize, returnColumnSizes);
+        used[i] = 0;
+    }
+}
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    *returnSize = 0;
+    int all =1;
+
+    for (int i=1; i<= numsSize; i++)
+        all*=i;
+    
+    int** result = malloc(all*sizeof(int*));
+    *returnColumnSizes = malloc(all*sizeof(int));
+    int* current = malloc(numsSize*sizeof(int));
+    int* used = calloc(numsSize, sizeof(int));
+
+    backtrack(nums, numsSize, current, 0, used, result, returnSize, returnColumnSizes);
+    free(current);
+    free(used);
+
+    return result;
+}
