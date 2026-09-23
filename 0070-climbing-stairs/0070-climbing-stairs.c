7ec814e38a7e9b28d8e1d493b89606144a9f48cf
Xenix04 <akshanshk0406@gmail.com>
2026-09-23 19:46:00 +0530

    Time: 0 ms (100%), Space: 8.5 MB (38.96%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,12 @@
+int climbStairs(int n) {
+    int p2 = 1, p1 =  1;
+
+    for ( int i = 2; i<=n; i++)
+    {
+        int curr = p2+p1;
+        p2 = p1;
+        p1 = curr;
+    }
+
+    return p1;
+}
